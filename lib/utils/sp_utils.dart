0e3e6aba740f0e28e3f3f989c8c71118681d1d06
robiriseup <robiul@riseuplabs.com>
2021-08-27 04:48:09 +0600

    initial commit

--- lib/utils/sp_utils.dart
+++ lib/utils/sp_utils.dart
@@ -10,23 +10,15 @@ class SPUtil {
   static String KEY_DARK_THEME = "KEY_DARK_THEME";
 
   setValue(String key, String value) async {
-    // SharedPreferences prefs = await SharedPreferences.getInstance();
     GetStorage().write(key, value);
   }
 
   deleteKey(String key) async {
-    // SharedPreferences prefs = await SharedPreferences.getInstance();
     GetStorage().remove(key);
   }
 
   String getValue(String key) {
     return GetStorage().read(key);
-    // SharedPreferences prefs = await SharedPreferences.getInstance();
-    // try {
-    //   var value = prefs.getString(key);
-    //   return value;
-    // } catch (error) {
-    //   return null;
-    // }
+
   }
 }
