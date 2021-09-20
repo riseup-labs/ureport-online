403510e8c3378f7475fb8542db67244bd5323cce
robiriseup <robiul@riseuplabs.com>
2021-09-20 13:14:14 +0600

    Opinion search page added

---
+++ lib/all-screens/home/opinion/opinion_search.dart
@@ -0,0 +1,287 @@
+import 'package:dotted_line/dotted_line.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:material_floating_search_bar/material_floating_search_bar.dart';
+import 'package:provider/provider.dart';
+import 'package:ureport_ecaro/all-screens/home/stories/model/searchbar.dart';
+import 'package:ureport_ecaro/locator/locator.dart';
+import 'package:ureport_ecaro/utils/nav_utils.dart';
+import 'package:ureport_ecaro/utils/resources.dart';
+import 'package:ureport_ecaro/utils/sp_utils.dart';
+
+import 'opinion_controller.dart';
+
+FloatingSearchBarController _floatingSearchBarController =
+FloatingSearchBarController();
+List<OpinionSearchList> filteredCategoryList = [];
+List<OpinionSearchList> categoryListFull = [];
+var isLoaded = true;
+
+class OpinionSearch extends StatefulWidget {
+  const OpinionSearch({Key? key}) : super(key: key);
+
+  @override
+  _OpinionSearchState createState() => _OpinionSearchState();
+}
+
+class _OpinionSearchState extends State<OpinionSearch> {
+  var sp = locator<SPUtil>();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Provider.of<OpinionController>(context, listen: false).isExpanded = false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Provider.of<OpinionController>(context, listen: false)
+        .getCategories(sp.getValue(SPUtil.PROGRAMKEY));
+
+    return Consumer<OpinionController>(builder: (context, provider, snapshot) {
+      return Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: SafeArea(
+          child: Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/bg_home.png"),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Container(
+              color: AppColors.bluelight,
+              height: 86,
+              child: Stack(
+                fit: StackFit.expand,
+                children: [
+                  Positioned(
+                    top: 12,
+                    left: 10,
+                    child: Container(
+                      width: 40,
+                      child: IconButton(
+                        icon: Icon(Icons.arrow_back),
+                        color: Colors.black,
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ),
+                  ),
+                  Container(
+                      margin: EdgeInsets.only(left: 40,right: 12,top: 10),
+                      child: searchBarUI(provider, sp.getValue(SPUtil.PROGRAMKEY))),
+                  Container(
+                    margin: EdgeInsets.only(top: 66),
+                    padding: EdgeInsets.only(left: 20, right: 20),
+                    child: FutureBuilder<List<OpinionSearchList>>(
+                        future: provider
+                            .getCategories(sp.getValue(SPUtil.PROGRAMKEY)),
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData && isLoaded) {
+                            filteredCategoryList = snapshot.data!;
+                            categoryListFull.addAll(snapshot.data!);
+                            isLoaded = false;
+                          }
+                          return Container(
+                            color: AppColors.white,
+                            child: ListView.builder(
+                              itemBuilder: (BuildContext context, int index) =>
+                                  DataPopUp(filteredCategoryList[index], provider),
+                              itemCount: filteredCategoryList.length,
+                            ),
+                          );
+                        }),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      );
+    });
+  }
+
+  Widget searchBarUI(OpinionController provider, String program) {
+    return FutureBuilder<List<OpinionSearchList>>(
+        future: provider.getCategories(program),
+        builder: (context, snapshot) {
+          return FloatingSearchBar(
+            hint: 'Search',
+            leadingActions: [Icon(Icons.search)],
+            openAxisAlignment: 0.0,
+            backdropColor: Colors.transparent,
+            backgroundColor: AppColors.white,
+            controller: _floatingSearchBarController,
+            height: 40.0,
+            elevation: 0.0,
+            axisAlignment: 0.0,
+            automaticallyImplyBackButton: false,
+            scrollPadding: EdgeInsets.only(bottom: 10, top: 5),
+            physics: BouncingScrollPhysics(),
+            onQueryChanged: (value) {
+              if(value == ""){
+                provider.setExpanded(false);
+              }else{
+                provider.setExpanded(true);
+              }
+              setState(() {
+                filteredCategoryList.clear();
+                for (var item in categoryListFull) {
+                  for (var child in item.children) {
+                    if (child.title
+                        .toLowerCase()
+                        .contains(value.toLowerCase())) {
+                      filteredCategoryList.add(item);
+                      break;
+                    }
+                  }
+                }
+              });
+            },
+            automaticallyImplyDrawerHamburger: false,
+            transitionCurve: Curves.easeInOut,
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(4),
+                bottomRight: Radius.circular(4)),
+            transitionDuration: Duration(milliseconds: 100),
+            transition: CircularFloatingSearchBarTransition(),
+            debounceDelay: Duration(milliseconds: 100),
+            actions: [],
+            builder: (context, transition) {
+              return ClipRRect(
+                borderRadius: BorderRadius.circular(8.0),
+                child: Material(
+                  color: Colors.white,
+                  child: Container(
+                    height: 1.0,
+                    color: Colors.transparent,
+                    child: ListView.builder(
+                      itemBuilder: (BuildContext context, int index) =>
+                          DataPopUp(filteredCategoryList[index], provider),
+                      itemCount: filteredCategoryList.length,
+                    ),
+                  ),
+                ),
+              );
+            },
+          );
+        });
+  }
+}
+
+Widget buildItem(OpinionSearchItem item, BuildContext context) {
+
+  final dateTime = DateTime.parse(item.date);
+  final format = DateFormat('dd MMMM, yyyy');
+  final titleDate = format.format(dateTime);
+
+  return Container(
+      child: GestureDetector(
+          onTap: () {
+            _floatingSearchBarController.clear();
+            _floatingSearchBarController.close();
+            // NavUtils.pushReplacement(
+            //     context,
+            //     OpinionDetails(item.id.toString(), item.title.toString(),
+            //         item.image,
+            //         item.date
+            //     ));
+          },
+          child: Padding(
+            padding: const EdgeInsets.only(left: 10, right: 17, bottom: 15),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Icon(Icons.arrow_right, size: 20,),
+                Expanded(child: Padding(
+                  padding: const EdgeInsets.only(top: 1),
+                  child: RichText(
+                    text: TextSpan(
+                      style: TextStyle(
+                          fontSize: 14.0,
+                          color: Colors.black,
+                          fontFamily: "Dosis"
+                      ),
+                      children: <TextSpan>[
+                        TextSpan(text: item.title),
+                        TextSpan(text: "  "),
+                        TextSpan(text: titleDate, style: new TextStyle(fontWeight: FontWeight.bold)),
+                      ],
+                    ),
+                  ),
+                )),
+              ],
+            ),
+          )
+      ));
+}
+
+class DataPopUp extends StatelessWidget {
+  const DataPopUp(this.popup, this.provider);
+
+  final OpinionSearchList popup;
+
+  final OpinionController provider;
+
+  Widget _buildTiles(OpinionSearchList root, BuildContext context) {
+    if (root.children.isEmpty) return ListTile(title: Text(root.title));
+
+    List<Widget> list = [];
+
+    for (var item in root.children) {
+      list.add(buildItem(item, context));
+    }
+
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 5, bottom: 3),
+          child: Theme(
+              data:  ThemeData().copyWith(dividerColor: Colors.transparent),
+              child: ExpansionTile(
+                key: PageStorageKey<OpinionSearchList>(root),
+                textColor: Colors.black,
+                maintainState: true,
+
+                collapsedTextColor: Colors.black,
+                trailing: Icon(Icons.arrow_drop_down),
+                title: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      root.title,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+
+                children: list,
+                initiallyExpanded: provider.isExpanded,
+                onExpansionChanged: (value){
+
+                },
+              ))
+          ,
+        ),
+        Container(
+          margin: EdgeInsets.only(left: 17, right: 17),
+          child: DottedLine(
+            dashColor: AppColors.primary,
+            dashLength: 2,
+          ),
+        )
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _buildTiles(popup, context);
+  }
+}
---
+++ lib/all-screens/home/opinion/statistics_location_spinner.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:percent_indicator/percent_indicator.dart';
+import 'package:provider/provider.dart';
+import 'model/response_opinions.dart' as questionArray;
+import 'model/response_opinions.dart';
+import 'opinion_controller.dart';
+
+class StatisticsLocationSpinner extends StatefulWidget {
+  questionArray.Question question;
+
+  StatisticsLocationSpinner(this.question);
+
+  @override
+  _StatisticsLocationSpinnerState createState() =>
+      _StatisticsLocationSpinnerState(question);
+}
+
+class _StatisticsLocationSpinnerState extends State<StatisticsLocationSpinner> {
+  questionArray.Question question;
+
+  _StatisticsLocationSpinnerState(this.question);
+
+  static List<MaterialColor> colors = [
+    Colors.lightBlue,
+    Colors.deepOrange,
+    Colors.amber,
+    Colors.teal
+  ];
+  
+  String dropdownValue = "";
+  late ResultsByLocation location;
+  var isLoaded = true;
+
+  @override
+  Widget build(BuildContext context) {
+    int colorNumber = 0;
+    List<DropdownMenuItem<String>> countryList = [];
+    List<String> countries = [];
+
+    question.resultsByLocation.forEach((element) {
+      countries.add(element.label);
+      countryList.add(DropdownMenuItem(
+        value: element.label,
+        child: Text(element.label),
+      ));
+    });
+
+    if(isLoaded){
+      location = question.resultsByLocation[0];
+      dropdownValue = countries[0];
+      isLoaded = false;
+    }
+    return Consumer<OpinionController>(builder: (context, provider, snapshot) {
+
+      return Column(
+        children: [
+          DropdownButton<String>(
+              value: dropdownValue,
+              iconSize: 24,
+              elevation: 16,
+              style:
+              TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+              onChanged: (String? newValue) {
+                  dropdownValue = newValue!;
+                  // print("the value is : $dropdownValue");
+                  question.resultsByLocation.forEach((element) { 
+                    if(element.label == dropdownValue){
+                      location = element;
+                    }
+                  });
+                  setState(() {});
+              },
+              items: countryList),
+          
+          ListView.builder(
+              shrinkWrap: true,
+              physics: BouncingScrollPhysics(),
+              itemCount: location.categories.length,
+              itemBuilder: (context, index) {
+                if (colorNumber > colors.length - 1) {
+                  colorNumber = 0;
+                }
+                int set = location.resultsByLocationSet;
+                int count = location.categories[index].count;
+                return Row(
+                  children: [
+                    Expanded(
+                      flex: 6,
+                      child: Container(
+                        child: LinearPercentIndicator(
+                          animation: false,
+                          lineHeight: 20.0,
+                          backgroundColor: Colors.white,
+                          percent: set != 0 ? count / set : 0.0,
+                          center: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Expanded(
+                                  child: Text(
+                                      "${location.categories[index].label}",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w700))),
+                            ],
+                          ),
+                          linearStrokeCap: LinearStrokeCap.roundAll,
+                          progressColor: colors[colorNumber++],
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Center(
+                        child: Container(
+                          margin: EdgeInsets.only(top: 4),
+                          child: Text(
+                              "${((set != 0 ? count / set : 0.0) * 100).round()}%"),
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              })
+        ],
+      );
+    });
+  }
+}
