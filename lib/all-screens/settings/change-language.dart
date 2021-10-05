13d7d2568de8befeb8913f0ed306ca5262051d39
robiriseup <robiul@riseuplabs.com>
2021-10-05 19:21:46 +0600

    Change language page edited

--- lib/all-screens/settings/change-language.dart
+++ lib/all-screens/settings/change-language.dart
@@ -33,6 +33,7 @@ class _ChangeLanguageState extends State<ChangeLanguage> {
     final provider = Provider.of<LocaleProvider>(context);
 
     String already_selected = sp.getValue(SPConstant.SELECTED_LANGUAGE);
+
     if (already_selected == "") {
       selectedValue = values.first;
     } else if (already_selected == "en") {
@@ -57,7 +58,7 @@ class _ChangeLanguageState extends State<ChangeLanguage> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             GestureDetector(
-              onTap: (){
+              onTap: () {
                 Navigator.pop(context);
                 ClickSound.buttonClickYes();
               },
@@ -87,6 +88,10 @@ class _ChangeLanguageState extends State<ChangeLanguage> {
     );
   }
 
+  Widget buildRadiosExtend(LocaleProvider provider) {
+    return Container();
+  }
+
   Widget buildRadios(LocaleProvider provider) => Container(
       padding: EdgeInsets.only(left: 30, right: 30),
       child: Card(
@@ -95,114 +100,148 @@ class _ChangeLanguageState extends State<ChangeLanguage> {
           borderRadius: BorderRadius.circular(10),
         ),
         margin: EdgeInsets.only(top: 10, bottom: 10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: values.map(
-            (value) {
-              return Padding(
-                padding: const EdgeInsets.all(5.0),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Expanded(
-                            flex: 4,
-                            child: Container(
-                                padding: EdgeInsets.only(left: 20),
-                                child: Text(
-                                  value,
-                                  style: TextStyle(
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.bold),
-                                ))),
-                        Expanded(
-                          flex: 1,
-                          child: Radio(
-                              value: value,
-                              groupValue: selectedValue,
-                              activeColor: RemoteConfigData.getPrimaryColor(),
-                              onChanged: (value) => {
-                                    setState(() => {
-                                          this.selectedValue = value.toString(),
-                                          if (this.selectedValue == values[0])
-                                            {
-                                              sp.setValue(
-                                                  SPConstant.SELECTED_LANGUAGE,
-                                                  "en"),
-                                              setLocal(provider)
-                                            }
-                                          else if (this.selectedValue ==
-                                              values[1])
-                                            {
-                                              sp.setValue(
-                                                  SPConstant.SELECTED_LANGUAGE,
-                                                  "zh"),
-                                              setLocal(provider)
-                                            }
-                                          else if (this.selectedValue ==
-                                              values[2])
-                                            {
-                                              sp.setValue(
-                                                  SPConstant.SELECTED_LANGUAGE,
-                                                  "fr"),
-                                              setLocal(provider)
-                                            }
-                                          else if (this.selectedValue ==
-                                              values[3])
-                                            {
-                                              sp.setValue(
-                                                  SPConstant.SELECTED_LANGUAGE,
-                                                  "ru"),
-                                              setLocal(provider)
-                                            }
-                                          else if (this.selectedValue ==
-                                              values[4])
-                                            {
-                                              sp.setValue(
-                                                  SPConstant.SELECTED_LANGUAGE,
-                                                  "es"),
-                                             setLocal(provider)
-                                            }
-                                        })
-                                  }),
-                        )
-                      ],
-                    ),
-                    SizedBox(height: 9,),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 20, right: 22),
-                      child: DottedLine(
-                        direction: Axis.horizontal,
-                        lineLength: double.infinity,
-                        lineThickness: .5,
-                        dashLength: 2.0,
-                        dashColor: Colors.lightBlueAccent,
-                        dashGapLength: 2.0,
-                        dashGapColor: Colors.transparent,
+        child: Container(
+          margin: EdgeInsets.only(top: 7),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: values.map(
+              (value) {
+                return Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Expanded(
+                              flex: 4,
+                              child: GestureDetector(
+                                onTap: () {
+                                  setState(() {
+                                    selectedValue = value;
+                                    print("Radio value : $value");
+                                    if (selectedValue == values[0]) {
+                                      sp.setValue(
+                                          SPConstant.SELECTED_LANGUAGE, "en");
+                                      setLocal(provider);
+                                    } else if (selectedValue == values[1]) {
+                                      sp.setValue(
+                                          SPConstant.SELECTED_LANGUAGE, "zh");
+                                      setLocal(provider);
+                                    } else if (this.selectedValue == values[2]) {
+                                      sp.setValue(
+                                          SPConstant.SELECTED_LANGUAGE, "fr");
+                                      setLocal(provider);
+                                    } else if (this.selectedValue == values[3]) {
+                                      sp.setValue(
+                                          SPConstant.SELECTED_LANGUAGE, "ru");
+                                      setLocal(provider);
+                                    } else if (this.selectedValue == values[4]) {
+                                      sp.setValue(
+                                          SPConstant.SELECTED_LANGUAGE, "es");
+                                      setLocal(provider);
+                                    }
+                                  });
+                                },
+                                child: Container(
+                                    padding: EdgeInsets.only(left: 20),
+                                    child: Text(
+                                      value,
+                                      style: TextStyle(
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.bold),
+                                    )),
+                              )),
+                          Expanded(
+                            flex: 1,
+                            child: Radio(
+                                value: value,
+                                groupValue: selectedValue,
+                                activeColor: RemoteConfigData.getPrimaryColor(),
+                                onChanged: (value) => {
+                                      setState(() => {
+                                            this.selectedValue = value.toString(),
+                                            if (this.selectedValue == values[0])
+                                              {
+                                                sp.setValue(
+                                                    SPConstant.SELECTED_LANGUAGE,
+                                                    "en"),
+                                                setLocal(provider)
+                                              }
+                                            else if (this.selectedValue ==
+                                                values[1])
+                                              {
+                                                sp.setValue(
+                                                    SPConstant.SELECTED_LANGUAGE,
+                                                    "zh"),
+                                                setLocal(provider)
+                                              }
+                                            else if (this.selectedValue ==
+                                                values[2])
+                                              {
+                                                sp.setValue(
+                                                    SPConstant.SELECTED_LANGUAGE,
+                                                    "fr"),
+                                                setLocal(provider)
+                                              }
+                                            else if (this.selectedValue ==
+                                                values[3])
+                                              {
+                                                sp.setValue(
+                                                    SPConstant.SELECTED_LANGUAGE,
+                                                    "ru"),
+                                                setLocal(provider)
+                                              }
+                                            else if (this.selectedValue ==
+                                                values[4])
+                                              {
+                                                sp.setValue(
+                                                    SPConstant.SELECTED_LANGUAGE,
+                                                    "es"),
+                                                setLocal(provider)
+                                              }
+                                          })
+                                    }),
+                          )
+                        ],
                       ),
-                    )
-                  ],
-                ),
-              );
-            },
-          ).toList(),
+                      SizedBox(
+                        height: 9,
+                      ),
+                      value != values.last?Padding(
+                        padding: const EdgeInsets.only(left: 20, right: 22),
+                        child: DottedLine(
+                          direction: Axis.horizontal,
+                          lineLength: double.infinity,
+                          lineThickness: .5,
+                          dashLength: 2.0,
+                          dashColor: Colors.lightBlueAccent,
+                          dashGapLength: 2.0,
+                          dashGapColor: Colors.transparent,
+                        ),
+                      ):Container()
+                    ],
+                  ),
+                );
+              },
+            ).toList(),
+          ),
         ),
       ));
 
   static void setLocal(LocaleProvider provider) {
     var sp = locator<SPUtil>();
-    if(sp.getValue(SPConstant.SELECTED_LANGUAGE) == "en"){
+    if (sp.getValue(SPConstant.SELECTED_LANGUAGE) == "en") {
       provider.setLocale(new Locale('en'));
-    }else if(sp.getValue(SPConstant.SELECTED_LANGUAGE) == "zh"){
+    } else if (sp.getValue(SPConstant.SELECTED_LANGUAGE) == "zh") {
       provider.setLocale(new Locale('zh'));
-    }else if(sp.getValue(SPConstant.SELECTED_LANGUAGE) == "fr"){
+    } else if (sp.getValue(SPConstant.SELECTED_LANGUAGE) == "fr") {
       provider.setLocale(new Locale('fr'));
-    }else if(sp.getValue(SPConstant.SELECTED_LANGUAGE) == "ru"){
+    } else if (sp.getValue(SPConstant.SELECTED_LANGUAGE) == "ru") {
       provider.setLocale(new Locale('ru'));
-    }else if(sp.getValue(SPConstant.SELECTED_LANGUAGE) == "es"){
+    } else if (sp.getValue(SPConstant.SELECTED_LANGUAGE) == "es") {
       provider.setLocale(new Locale('es'));
-    }else{
+    } else {
       provider.setLocale(new Locale('en'));
     }
   }
